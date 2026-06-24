491cdbb59a7b14595dc36079cd42a701fcedfabd
Zachary Holmes <zack.holmes2003@gmail.com>
2026-06-23 21:31:34 -0400

    fixed the starting opener

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ namespace DisciplineDashboard
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Dashboard}/{action=Index}/{id?}")
+                pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
             app.MapRazorPages()
                .WithStaticAssets();
